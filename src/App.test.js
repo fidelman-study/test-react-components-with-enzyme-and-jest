99088a5dd89798ac48df24b8c9c499bc3b867e18
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-26 15:06:44 +0100

    Fully Render React Components with Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import App, { Link } from './App'
-import { configure, shallow } from 'enzyme'
+import { configure, shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
-describe('<App/>', () => {
+describe('<App/> shallow rendering', () => {
   it('should contain p element', () => {
     const wrapper = shallow(<App/>)
     expect(wrapper.find('p').exists()).toBe(true)
+  })
+})
+
+describe('<App/> mount rendering', () => {
+  it('should contain p element', () => {
+    const wrapper = mount(<App/>)
+    expect(wrapper.find('p').exists()).toBe(true)
+    wrapper.unmount()
   })
 })
 
