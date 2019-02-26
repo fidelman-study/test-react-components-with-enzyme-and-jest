a5ac4144e55624ef3f92d425f14cd58896f895dd
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-26 14:47:26 +0100

    Test React Component Props with Enzyme and Jest

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita inventore corporis maxime delectus at labore vel culpa iste quas tenetur iusto excepturi ad ipsa temporibus, recusandae aspernatur corrupti quo molestias.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          inventore corporis maxime delectus at labore vel culpa iste quas
+          tenetur iusto excepturi ad ipsa temporibus, recusandae aspernatur
+          corrupti quo molestias.
+        </p>
       </div>
-    )
+    );
   }
 }
+
+export const Link = ({ address, hide }) =>
+  hide ? null : <a href={address}>Click</a>;
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import App from './App'
+import App, { Link } from './App'
 import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
 describe('<App/>', () => {
-  const wrapper = shallow(<App/>)
   it('should contain p element', () => {
+    const wrapper = shallow(<App/>)
     expect(wrapper.find('p').exists()).toBe(true)
   })
+})
+
+describe('<Link/>', () => {
+  it('a tag node render href correctly', () => {
+    const address = 'www.google.com'
+    const wrapper = shallow(<Link address={address}/>)
+    expect(wrapper.props().href).toBe(address)
+  })
+
+  it('should return null with true hide prop', () => {
+    const wrapper = shallow(<Link hide={false}/>)
+    expect(wrapper.find('a').length).toBe(1)
+    wrapper.setProps({ hide: true })
+    expect(wrapper.get(0)).toBeNull()
+  })
+
 })
